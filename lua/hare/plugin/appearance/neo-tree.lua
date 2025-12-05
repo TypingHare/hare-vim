3ab1b19b84cc4c9812b6f64df2b9a313779632b2
James Chen <jameschan312.cn@gmail.com>
2025-12-05 17:01:32 -0500

    feat: hide "(x hidden item)" in neo-tree

--- lua/hare/plugin/appearance/neo-tree.lua
+++ lua/hare/plugin/appearance/neo-tree.lua
@@ -20,6 +20,7 @@ return {
         filesystem = {
             group_empty_dirs = true,
             filtered_items = {
+                show_hidden_count = false,
                 hide_dotfiles = true,
                 hide_gitignored = true,
                 always_show = { '.gitignore', '.gitkeep' },
