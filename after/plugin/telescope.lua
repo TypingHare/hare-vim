6cb9c2732c5f47128f39d517e5ae7e779c4ac2a4
James Chen <jameschan312.cn@gmail.com>
2026-06-15 20:49:32 -0400

    feat: add `telescope` related keybindings

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,77 @@
+local ok, telescope = pcall(require, 'telescope')
+if not ok then
+    return
+end
+
+telescope.setup {
+    defaults = {
+        layout_strategy = 'horizontal',
+        layout_config = {
+            prompt_position = 'top',
+            preview_width = 0.6,
+        },
+        sorting_strategy = 'ascending',
+        scroll_strategy = 'cycle',
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+        oldfiles = {
+            hidden = true,
+        },
+    },
+}
+
+-- Find files in the project scope.
+vim.keymap.set(
+    'n',
+    '<leader>ff',
+    ':Telescope find_files<CR>',
+    { desc = 'Find Files', silent = true }
+)
+
+-- Find buffers.
+vim.keymap.set(
+    'n',
+    '<leader>fb',
+    ':Telescope buffers<CR>',
+    { desc = 'Find Buffers', silent = true }
+)
+
+-- Find old files in the project scope. Old files refer to the files that were
+-- opened and recorded before by Neovim.
+vim.keymap.set('n', '<leader>fo', function()
+    require('telescope.builtin').oldfiles { cwd_only = true }
+end, { desc = 'Find Old Files', silent = true })
+
+-- Find files that have the same extension as the file in the current buffer.
+vim.keymap.set('n', '<leader>ft', function()
+    local ext = vim.fn.expand '%:e'
+    require('telescope.builtin').find_files {
+        find_command = { 'fd', '--type', 'f', '--extension', ext },
+        prompt_title = 'Files with extension: ' .. ext,
+    }
+end, { desc = 'Find Files with Same Extension', silent = true })
+
+-- Find files by matching specific content.
+vim.keymap.set(
+    'n',
+    '<leader>fg',
+    ':Telescope live_grep<CR>',
+    { desc = 'Live Grep', silent = true }
+)
+
+-- Go to the definition under the cursor.
+vim.keymap.set(
+    'n',
+    'gd',
+    require('telescope.builtin').lsp_definitions,
+    { desc = 'Go to Definition', silent = true }
+)
+
+-- Go to the reference (implementation) under the cursor.
+vim.keymap.set('n', 'grr', require('telescope.builtin').lsp_references, {
+    desc = 'Go to Reference',
+    silent = true,
+})
