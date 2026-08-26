255f7647588dc605c73b4a438d33a6e42d196fa0
James Chen <jameschen312.us@gmail.com>
2026-08-25 23:36:34 -0400

    feat: add the `nvim-treesitter/nvim-treesitter-textobjects` plugin

--- lua/hare/plugin/coding.lua
+++ lua/hare/plugin/coding.lua
@@ -8,4 +8,42 @@ return {
         build = ':TSUpdate',
         event = { 'BufReadPost', 'BufNewFile' },
     },
+
+    -- This plugin adds syntax-aware text objects and motions to Neovim using
+    -- Tree-sitter, so you can target structures like functions, classes,
+    -- parameters, and loops.
+    --
+    -- [https://github.com/nvim-treesitter/nvim-treesitter-textobjects]
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        opts = {
+            select = {
+                lookahead = true,
+                selection_modes = {
+                    ['@function.outer'] = 'V',
+                    ['@function.inner'] = 'V',
+                },
+            },
+        },
+        keys = {
+            {
+                'af',
+                function()
+                    local m = require 'nvim-treesitter-textobjects.select'
+                    m.select_textobject('@function.outer', 'textobjects')
+                end,
+                mode = { 'x', 'o' },
+                desc = 'Around function',
+            },
+            {
+                'if',
+                function()
+                    local m = require 'nvim-treesitter-textobjects.select'
+                    m.select_textobject('@function.inner', 'textobjects')
+                end,
+                mode = { 'x', 'o' },
+                desc = 'Inside function',
+            },
+        },
+    },
 }
