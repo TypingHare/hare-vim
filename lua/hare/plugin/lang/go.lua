2ec722d83c3cd1fdc3b86233a243b89757efdf1f
James Chen <jameschen312.us@gmail.com>
2026-09-06 09:46:04 -0400

    feat: add plugin `ray-x/go.nvim`; update plugins

---
+++ lua/hare/plugin/lang/go.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        'ray-x/go.nvim',
+        dependencies = {
+            'ray-x/guihua.lua',
+            'neovim/nvim-lspconfig',
+        },
+        config = function()
+            require('go').setup {
+                lsp_inlay_hints = {
+                    enable = false,
+                },
+            }
+        end,
+        event = { 'CmdlineEnter' },
+        ft = { 'go', 'gomod' },
+        build = ':lua require("go.install").update_all_sync()',
+    },
+}
